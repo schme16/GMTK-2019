fe20c1b1b36862276195ec714da791195f013ad9
Shane Gadsby <schme16@gmail.com>
2019-08-03 14:46:22 +1000

    first push

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express'),
+	compression = require('compression'),
 	app = express()
 
 
+app.use(compression({level:9}))
 
 app.use('/node_modules', express.static('node_modules'))
+
 app.use(express.static('public'))
 
 
